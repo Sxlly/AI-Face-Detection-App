8cd0a497605503708f6a04b44d6a6ce4d8eaf3fe
Sxlly <shaesullivan.buisness1@gmail.com>
2024-05-03 16:24:53 +0800

    drawing rectangle around detected face

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ import cv2
 
 
 #*** pre made dataset import
-preTrainedFaceData = cv2.CascadeClassifier('enter xml machine learning file') #<---- load pre-trained dataset on front facing self portraits 
+preTrainedFaceData = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') #<---- load pre-trained dataset on front facing self portraits 
 
 
 #*** choose image to detect a face inside
@@ -15,12 +15,14 @@ gsImg = cv2.cvtColor(initImg, cv2.COLOR_BGR2GRAY) #<---- second parameter sets i
 #*** Detect Facial features within image
 faceCoordinates = preTrainedFaceData.detectMultiScale(gsImg) #<--- the classifier trained the "preTrainedFaceData" variable to detect faces accuratley due to funneling it multiple positive and negative data sets (imgs with faces and without)
                                     #^^^ multiscale method allows images to be various dimensions (scales)
+print(faceCoordinates)#<--- prints coordinates of facial borders to console (terminal)
+
 
 #*** Draw Rectangle around detected face
-#cv2.rectangle()
+cv2.rectangle()  #<--- (img, (x, y), (x+w, y+w), (B, G, R), BorderWidth)
 
 
 
 #*** display in image view grayscaled image
-cv2.imshow('AI Face detector', gsImg ) #<--- parameter 1 is AppBar title and 2 is Image to Open
+cv2.imshow('AI Face detector', initImg) #<--- parameter 1 is AppBar title and 2 is Image to Open
 cv2.waitKey() #<--- waitKey to prevent photo viewer from instantly exiting
