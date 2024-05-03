3dde04c2ed49f9f546b25cf4de50f7daac70dfab
Sxlly <shaesullivan.buisness1@gmail.com>
2024-05-03 15:40:37 +0800

    importing image to read

--- Main.py
+++ Main.py
@@ -3,4 +3,12 @@ import cv2
 
 
 
-preTrainedFaceData = cv2.CascadeClassifier('enter xml machine learning file')
+preTrainedFaceData = cv2.CascadeClassifier('enter xml machine learning file') #<---- load pre-trained dataset on front facing self portraits 
+
+
+#*** Choose image to detect a face inside
+initImg = cv2.imread('filename.png/jpg')
+
+#*** for opencv to use image correctly mustfirst convert to grayscale
+gsImg = cv2.cvtColor(initImg, cv2.COLOR_BGR2GRAY) #<---- second parameter sets img background to gray
+
